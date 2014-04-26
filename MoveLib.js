e7b4d5ef9d4c89cf1279e300153c221a030f2003
James Storey <storey.james@gmail.com>
2014-04-26 16:58:26 -0500

    event based movement scheme

--- MoveLib.js
+++ MoveLib.js
@@ -3,7 +3,11 @@
 var MoveLib = function () {
 	var that = {};
 
-	var PaceH = function (property) {
-		
+	var PaceH = function (context) {
+		game.time.events.add(500, context.move, context, 1, 0);
+		game.time.events.add(1500, context.move, context, 0, 0);
 	};
+
+	that.PaceH = PaceH;
+	return that;
 }();
--- Person.js
+++ Person.js
@@ -1,7 +1,6 @@
 
 var Person = function (x, y) {
 	var that = {};
-
 	Entity(that);
 
 	var shape = game.add.sprite(60, 60, 'redBox');
@@ -9,12 +8,43 @@ var Person = function (x, y) {
 	shape.position.y = y;
 	shape.anchor.setTo(0.5, 0.5);
 
-	//game.physics.enable(shape, Phaser.Physics.ARCADE);
+	var moveDir = new Phaser.Point(0,0);
+	var lookDir = lookState.down;
+	var speed = 1;
+	var moving = false
+	var movePattern = [];
+
+	var move = function(x, y) {
+		moving = true;
+		moveDir = new Phaser.Point(x,y).normalize();
+		if(moveDir.x > 0 && moveDir.x > y)
+		{
+			lookDir = lookState.right;
+		}
+		else if(moveDir.x < 0 && moveDir.x < moveDir.y)
+		{
+			lookDir = lookState.left;
+		}
+		else if(moveDir.y > 0 && moveDir.y > moveDir.x)
+		{
+			lookDir = lookState.up;
+		}
+		else
+		{
+			lookDir = lookState.down;
+		}
+	};
 
 	that.update = function () {
-		
+		//if(moving)
+		//{
+		shape.position.x += moveDir.x*speed;
+		shape.position.y += moveDir.y*speed;
+		//}
 	};
 
 	that.shape = shape;
+	that.move = move;
+	MoveLib.PaceH(that);
 	return that;
 };
