166bce365d21a909b845a5d52e8a1728c2c97032
James Storey <storey.james@gmail.com>
2014-04-27 18:49:59 -0500

    mapped out security and terminal

--- ScriptEvents.js
+++ ScriptEvents.js
@@ -27,7 +27,63 @@ var Opening = function () {
 		// move to hall entry
 
 		// switch to move on
-		
+
+	}
+
+	that.start = start;
+
+	return that; 
+}();
+
+var Security = function () {
+	var that = {};
+
+	var securityGuard;
+	var startPerson;
+	var repeatable;
+
+	var start = function() 
+	{
+		// set persons
+
+		// move to queue
+
+		// walk to scanner
+
+		// sound off scanner
+
+		// loop checking until player is not on person
+
+		// loop repeatable until leaves on person
+
+	}
+
+	that.start = start;
+
+	return that; 
+}();
+
+var Security = function () {
+	var that = {};
+
+	var startPerson;
+	var hatMan;
+
+	var start = function() 
+	{
+		// set persons
+
+		// move to ticker
+
+		// quip about delay
+
+		// move to loop
+
+		// make loop
+
+		// hatMan is seen and moves to bathroom
+
+		// loop until nudge to bathroom
 	}
 
 	that.start = start;
