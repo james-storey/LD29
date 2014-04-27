4897800c0482419c448b3d755af0273aa9546ea9
James Storey <storey.james@gmail.com>
2014-04-27 18:29:28 -0500

    mapped Opening

--- ScriptEvents.js
+++ ScriptEvents.js
@@ -2,8 +2,31 @@
 var Opening = function () {
 	var that = {};
 
+	var adam;
+	var eve;
+	var attendant;
+	var linePerson;
+
 	var start = function() 
 	{
+		adam = characters[0];
+		eve = characters[1];
+		attendant = minorCharacters[0];
+		linePerson = minorCharacters[1];
+
+		// pan camera
+
+		// adam, eve discuss
+
+		// line moves
+
+		// nudge to move on
+
+		// get pass
+
+		// move to hall entry
+
+		// switch to move on
 		
 	}
 
