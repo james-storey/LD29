1fc4169a9f01b8b6aff4661b1852753dd1e21c91
Thomas Storey <storey.thomas@gmail.com>
2014-04-28 01:24:38 -0400

    security terminal sprites

--- Program.js
+++ Program.js
@@ -32,7 +32,7 @@ var Program = function () {
 		game.load.image("redBox", "resources/redBox.png");
 		game.load.image("lobby", "character_sprites/lobbypixel.png");
 		game.load.image("security", "character_sprites/security.png");
-		game.load.image("layout", "character_sprites/layout.png");
+		game.load.image("layout", "character_sprites/layoutNoFront.png");
 
 		game.load.json("thoughts", "resources/thoughts.json");
 	};
